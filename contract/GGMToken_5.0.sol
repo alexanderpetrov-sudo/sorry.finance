cfa2d8eb8575f1fdb7849452bb72183aed180d61
alexanderpetrov-sudo <84181608+alexanderpetrov-sudo@users.noreply.github.com>
2021-05-14 14:06:50 +0300

    Update GGMToken_5.0.sol

--- contract/GGMToken_5.0.sol
+++ contract/GGMToken_5.0.sol
@@ -1,12 +1,12 @@
 pragma solidity ^0.5.00;
 
 // ----------------------------------------------------------------------------
-// 'GGMToken' token contract
+// 'SORRYFINANCE' token contract
 //
-// Deployed to : 0xFEB02D9383C49A8373F88e82EbCecB553c1837bf
-// Symbol      : GGM
-// Name        : GGMToken
-// Total supply: 100000000
+// Deployed to : 0x4672fFcAf81834abB766932A00e7cE9aFC46bC70
+// Symbol      : SORRY
+// Name        : SORRYFinance
+// Total supply: 5000000000000000
 // Decimals    : 18
 //
 // Enjoy.
@@ -99,7 +99,7 @@ contract Owned {
 // ERC20 Token, with the addition of symbol, name and decimals and assisted
 // token transfers
 // ----------------------------------------------------------------------------
-contract GGMToken is ERC20Interface, Owned, SafeMath {
+contract SORRYFinance is ERC20Interface, Owned, SafeMath {
     string public symbol;
     string public  name;
     uint8 public decimals;
@@ -113,12 +113,12 @@ contract GGMToken is ERC20Interface, Owned, SafeMath {
     // Constructor
     // ------------------------------------------------------------------------
     constructor() public {
-        symbol = "GGM";
-        name = "GGMToken";
+        symbol = "SORRY";
+        name = "SORRYFinance";
         decimals = 18;
         _totalSupply = 100000000000000000000000000;
-        balances[0xFEB02D9383C49A8373F88e82EbCecB553c1837bf] = _totalSupply;
-        emit Transfer(address(0), 0xFEB02D9383C49A8373F88e82EbCecB553c1837bf, _totalSupply);
+        balances[0x4672fFcAf81834abB766932A00e7cE9aFC46bC70] = _totalSupply;
+        emit Transfer(address(0), 0x4672fFcAf81834abB766932A00e7cE9aFC46bC70, _totalSupply);
     }
 
 
