e56ca1a8d0a6bda91ba31f34ece139caf1362c84
alexanderpetrov-sudo <84181608+alexanderpetrov-sudo@users.noreply.github.com>
2021-06-07 15:02:58 +0300

    Update SORRYFinance_6.12.sol

--- contract/SORRYFinance_6.12.sol
+++ contract/SORRYFinance_6.12.sol
@@ -5,8 +5,7 @@
    #SORRY.finance features:
    3% fee auto add to the liquidity pool to locked forever when selling
    2% fee auto distribute to all holders
-   I created a black hole so #Bee token will deflate itself in supply with every transaction
-   50% Supply is burned at start.
+   Whales cannot dump more than 20% every 15 minutes. Make sure you have alerts on trust wallet ON (in case this happens)
    
 
  */
